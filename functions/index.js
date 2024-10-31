b9e0fe720d2b9a4d90b5b8ed6849758f50272aee
aalej <alejandromarco@google.com>
2024-11-01 00:30:13 +0800

    revert changes to function name

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions/v2';
 
-export const adminV1 = functions.tasks.onTaskDispatched(
+export const admin_V1 = functions.tasks.onTaskDispatched(
     {
         secrets: ['SECRET'],
         rateLimits: {},
